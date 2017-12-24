c0593b0f50fc445b5fee41053e3aed473a48980f
Boyd Multerer <boyd@multerer.com>
2017-12-24 01:23:28 -0800

    minor cleanup

--- lib/phoenix_markdown/engine.ex
+++ lib/phoenix_markdown/engine.ex
@@ -1,14 +1,13 @@
 defmodule PhoenixMarkdown.Engine do
   @behaviour Phoenix.Template.Engine
 
-  import IEx
-
   @doc """
   Precompiles the String file_path into a function defintion, using the EEx and Earmark engines
   """
   def compile(path, _name) do
     md = File.read!(path)
     |> Earmark.as_html!()
+    
     if Application.get_env(:phoenix_markdown, :smart_tags) do
       restore_smart_tags(md)
       |> EEx.compile_string(engine: EEx.SmartEngine, file: path, line: 1)
